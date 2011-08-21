302fbbc9ace82a71c0ad1511a8e8d9555e247c15
dbuckler <db@intuvo.com>
2011-08-21 16:05:32 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '35a6acaed8a3f65bc40c6dffed36fedd6de79fc65080cf62eb9d13ffeef384e721b6bc55b42ad27c0aed8735f883ba2728bfd27ab0a3671924d6c1c8f1dc2ee0'
